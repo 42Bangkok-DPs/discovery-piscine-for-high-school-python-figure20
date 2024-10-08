8dc24eed9ccaa0398c60367a68e169bed9f255f6
figure20 <figure.sirach@gmail.com>
2024-10-08 09:54:36 +0700

    Create mult.py

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,9 @@
+x = int(input('Enter the first number:')) 
+y = int(input('Enter the second number:'))
+print(x,'X', y , ' =',x * y)
+if x * y > 0: 
+    print("The result is positive.")
+elif x * y < 0: 
+    print("The result is negative.")
+else: 
+    print("This result is both positive and negative.")
